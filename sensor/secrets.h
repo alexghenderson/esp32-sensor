43737ae70504e97ce886560b018fe7a8624a4c00
Alex Henderson <alex@alexhenderson.me>
2025-03-07 18:38:36 -0800

    move secrets.h to secrets.h.example

--- sensor/secrets.h
+++
@@ -1,9 +0,0 @@
-// Wifi Credentials
-const char* ssid = "wifi ssid";
-const char* password = "wifi password";
-
-// Server URL (HTTPS)
-const char* serverName = "https://example.com/ingest";
-
-// Private Key for signing ingest requests
-const char* private_key = "xxxxxx"
